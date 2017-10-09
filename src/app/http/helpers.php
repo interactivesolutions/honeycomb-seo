1dbcfb2a315611718b1e41758e742e92e4227e0d
Mantas Paulauskas <mantopastas@gmail.com>
2017-10-09 17:43:13 +0300

    formmanager seo fields helper function

--- src/app/http/helpers.php
+++ src/app/http/helpers.php
@@ -1,1 +1,38 @@
 <?php
+
+if( ! function_exists('formManagerSeoFields') ) {
+
+    /**
+     * Adding seo fields (title, description, keywords
+     * used by Form-Managers
+     *
+     * @param array $list
+     * @return mixed
+     */
+    function formManagerSeoFields(array &$list)
+    {
+        $list['structure'] = array_merge($list['structure'], [
+            [
+                "type"      => "singleLine",
+                "fieldID"   => "seo.seo_title",
+                "label"     => trans("HCTranslations::core.seo_title"),
+                "tabID"     => trans('HCTranslations::core.seo'),
+                "maxLength" => 70,
+            ],
+            [
+                "type"      => "textArea",
+                "fieldID"   => "seo.seo_description",
+                "label"     => trans("HCTranslations::core.seo_description"),
+                "tabID"     => trans('HCTranslations::core.seo'),
+                "rows"      => 5,
+                "maxLength" => 155,
+            ],
+            [
+                "type"    => "singleLine",
+                "fieldID" => "seo.seo_keywords",
+                "label"   => trans("HCTranslations::core.seo_keywords"),
+                "tabID"   => trans('HCTranslations::core.seo'),
+            ],
+        ]);
+    }
+}
